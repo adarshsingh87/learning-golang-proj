6cbaac9deac5b042e88f8f1b151cb1d496c4839c
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-01-05 23:38:40 +0530

    feat: form handler func

--- simple-webserver/main.go
+++ simple-webserver/main.go
@@ -6,7 +6,22 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+	
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
@@ -18,6 +33,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Hello! World")
+
 }
 
 func main() {
@@ -25,6 +41,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 
 	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
