0084b973e09fb3581dfc4ba4e3b6692dd25c87f0
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-01-07 00:33:59 +0530

    feat: get funciton

--- movie-crud-api/main.go
+++ movie-crud-api/main.go
@@ -1,52 +1,57 @@
 package main
 
-import(
+import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
 type Movie struct {
-	Id string `json:"id"`
-	Isbn string `json:"isbn"`
-	Title string `json:"title"`
+	Id       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
 	Director *Director `json:"director"`
 }
 
 type Director struct {
 	FirstName string `json:"firstName"`
-	LastName string `json:"lastName"`
+	LastName  string `json:"lastName"`
 }
 
 var movies []Movie // using slice(db baadme seekh lunga)
 
-func getMovies(w http.ResponseWriter, r *http.Request) {}
-func getMovie(w http.ResponseWriter, r *http.Request) {}
-func addMovie(w http.ResponseWriter, r *http.Request) {}
-func editMovie(w http.ResponseWriter, r *http.Request) {}
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
 func deleteMovie(w http.ResponseWriter, r *http.Request) {}
+func getMovie(w http.ResponseWriter, r *http.Request)    {}
+func addMovie(w http.ResponseWriter, r *http.Request)    {}
+func editMovie(w http.ResponseWriter, r *http.Request)   {}
 
-func main () {
+func main() {
 	r := mux.NewRouter()
 
 	// appending to the first get request works
 	movies = append(movies, Movie{
-		Id: "1",
-		Isbn: "216039",
+		Id:    "1",
+		Isbn:  "216039",
 		Title: "Demon Slayer: Infinity train",
 		Director: &Director{
 			FirstName: "Haruo",
-			LastName: "Sotozaki",
+			LastName:  "Sotozaki",
 		},
 	})
 	movies = append(movies, Movie{
-		Id: "2",
-		Isbn: "720149",
+		Id:    "2",
+		Isbn:  "720149",
 		Title: "jujutsu kaisen 0",
 		Director: &Director{
 			FirstName: "Sunghoo",
-			LastName: "Park",
+			LastName:  "Park",
 		},
 	})
 
