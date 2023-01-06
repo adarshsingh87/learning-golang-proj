0378a64ac5121d2913ef805f6342e9e9a1a7f537
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-01-07 00:22:01 +0530

    feat: added router

--- movie-crud-api/main.go
+++ movie-crud-api/main.go
@@ -1,5 +1,12 @@
 package main
 
+import(
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
 type Movie struct {
 	Id string `json:"id"`
 	Isbn string `json:"isbn"`
@@ -10,4 +17,27 @@ type Movie struct {
 type Director struct {
 	FirstName string `json:"firstName"`
 	LastName string `json:"lastName"`
+}
+
+var movies []Movie // using slice(db baadme seekh lunga)
+
+func getMovies(w http.ResponseWriter, r *http.Request) {}
+func getMovie(w http.ResponseWriter, r *http.Request) {}
+func addMovie(w http.ResponseWriter, r *http.Request) {}
+func editMovie(w http.ResponseWriter, r *http.Request) {}
+func deleteMovie(w http.ResponseWriter, r *http.Request) {}
+
+func main () {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", addMovie).Methods("POST")
+	r.HandleFunc("/movies", editMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
