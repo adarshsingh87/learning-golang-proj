e10c8bbb4e3df5c19bfe4354a2fb51ddda3fcb0d
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-01-07 00:13:59 +0530

    feat: movie struct

---
+++ movie-crud-api/main.go
@@ -0,0 +1,13 @@
+package main
+
+type Movie struct {
+	Id string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstName"`
+	LastName string `json:"lastName"`
+}
