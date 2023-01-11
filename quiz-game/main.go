1817fe5863846f99129cd999e9e10b40a275395c
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-01-11 23:02:16 +0530

    feat: quiz game basic done

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -9,6 +9,7 @@ func main(){
 
 	var name string
 	var age uint // uint so no negative values
+	score :=0
 
 	fmt.Print("Enter your name: ")
 	fmt.Scan(&name)
@@ -23,5 +24,36 @@ func main(){
 		fmt.Println("Sorry, you are too young to play our game")
 		return
 	}
-		fmt.Println("Test")
+
+	var cores int
+	fmt.Print("How many cores does Ryzen 9 3900x have: ")
+	fmt.Scan(&cores)
+
+	if cores == 12 {
+		fmt.Println("Correct! ")
+		score++
+	} else {
+		fmt.Printf("Incorrect, score %v", score)
+		return
+	}
+
+	var fastestGpu1 string
+	var fastestGpu2 string
+
+	fmt.Print("What is the fastest 40Series Nvidia GPU: ")
+	fmt.Scan(&fastestGpu1, &fastestGpu2)
+
+	if fastestGpu1 + " " + fastestGpu2 == "RTX 4090" {
+		fmt.Println("Correct! ")
+		score++
+	} else if fastestGpu1 + " " + fastestGpu2 == "rtx 4090" {
+		fmt.Println("Correct! ")
+		score++
+	}	else {
+		fmt.Printf("Incorrect, score %v", score)
+		return
+	}
+
+	fmt.Printf("score: %v", score)
+
 }
