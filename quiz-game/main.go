9af3c6256bb71f18cfc5960b03091690518841ee
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-01-11 22:45:05 +0530

    feat: prompt

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -6,4 +6,22 @@ import(
 
 func main(){
 	fmt.Println("Welcome to the quiz game!")
+
+	var name string
+	var age uint // uint so no negative values
+
+	fmt.Print("Enter your name: ")
+	fmt.Scan(&name)
+
+	fmt.Printf("Hi %v, welcome to the game! \n", name)
+
+	fmt.Printf("%v, how old are you? ", name)
+
+	fmt.Scan(&age)
+
+	if age < 15 {
+		fmt.Println("Sorry, you are too young to play our game")
+		return
+	}
+		fmt.Println("Test")
 }
