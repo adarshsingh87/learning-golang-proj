aa929abf378e74d82b1204b3a304a8d74883da5c
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-01-11 23:18:12 +0530

    feat: router defination

--- gin-server/main.go
+++ gin-server/main.go
@@ -12,3 +12,25 @@ type book struct {
 	Author   string `json:"author"`
 	Quantity int    `json:"quantity"`
 }
+
+var books = []book{
+	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
+
+func getBooks(){}
+func bookById(){}
+func createBook(){}
+func checkoutBook(){}
+func returnBook(){}
+
+func main() {
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.GET("/books/:id", bookById)
+	router.POST("/books", createBook)
+	router.PATCH("/checkout", checkoutBook)
+	router.PATCH("/return", returnBook)
+	router.Run("localhost:8080")
+}
